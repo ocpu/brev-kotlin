2627f331934685c47645afe77ffc6010991c499b
ocpu <martin.hovre@opencubes.io>
2018-09-12 11:27:49 +0200

    Shortend the subscription part

--- examples/src/SubscribeExample.kt
+++ examples/src/SubscribeExample.kt
@@ -4,17 +4,13 @@ object SubscribeExample {
   @JvmStatic fun main(args: Array<String>) {
 //    val subscriptionBus = Brev.createBus()
     val subscriptionBus = Brev
-    SubscribeExample::class
-        .java
-        .methods
+    SubscribeExample::class.java.methods
         .filter { it.isAnnotationPresent(Subscribe::class.java) }
         .forEach { method ->
-          val event = method.parameterTypes[0] as Class<IEvent>
-          val limit = method.getAnnotation(Subscribe::class.java).value
-          val listener: (IEvent) -> Unit = { method(SubscribeExample, it) }
-
-          subscriptionBus.many(event, limit, listener)
-          Unit
+          subscriptionBus.many(
+              method.parameterTypes[0] as Class<IEvent>, // event
+              method.getAnnotation(Subscribe::class.java).value // limit (0 = never remove)
+          ) { event -> method.invoke(SubscribeExample, event) } // listener
         }
     Brev.emit(MyEvent("World"))
     Brev.emit(MyEvent("Eva"))
@@ -22,7 +18,7 @@ object SubscribeExample {
   }
 
   @Subscribe
-  fun greatings(e: MyEvent) {
+  fun greetings(e: MyEvent) {
     println("Greetings ${e.value}!")
   }
 
