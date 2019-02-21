c921e6f28d11557cd320eb7a8a8101227968e164
ocpu <martin.hovre@opencubes.io>
2019-02-21 17:51:48 +0100

    Fixed comment typos

--- src/main/kotlin/io/opencubes/brev/util.kt
+++ src/main/kotlin/io/opencubes/brev/util.kt
@@ -2,11 +2,11 @@
 
 package io.opencubes.brev
 
-/**A shorthand function forregistering on events. */
+/**A shorthand function for registering on events. */
 inline fun <reified T : Any> IMessageBus<in T>.on(noinline listener: (event: T) -> Unit) = on(T::class.java, listener)
-/**A shorthand function forregistering once on events. */
+/**A shorthand function for registering once on events. */
 inline fun <reified T : Any> IMessageBus<in T>.once(noinline listener: (event: T) -> Unit) = once(T::class.java, listener)
-/**A shorthand function forregistering on events. */
+/**A shorthand function for registering on events. */
 inline fun <reified T : Any> IMessageBus<in T>.many(limit: Int, noinline listener: (event: T) -> Unit) = many(T::class.java, limit, listener)
 /**A shorthand function unregistering off events. */
 inline fun <reified T : Any> IMessageBus<in T>.off(noinline listener: (event: T) -> Unit) = off(T::class.java, listener)
